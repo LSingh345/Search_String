96b8a37b5ad7698b196b5811d56b500d4fb36488
Lakshya Singh <lakshya.nauhwar@gmail.com>
2025-07-01 18:58:15 -0400

    Update test.py

--- programs/test.py
+++ programs/test.py
@@ -1,1 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
 
+        return " ".join(list(reversed([x for x in s.split(' ') if x != ""])))
